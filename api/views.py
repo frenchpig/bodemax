1ebdb601955310dd65e0c6485ac3a9120d72df40
frenchpig <auspont.javier@gmail.com>
2024-05-04 16:07:39 -0400

    CRUD de item completo Se agregan la funcionalidades de PUT y DELETE a la api por lo que ahora se puede realizar metodos POST, GET, PUT y DELETE para los items asociados a la API_KEY del usuario.

--- api/views.py
+++ api/views.py
@@ -15,6 +15,7 @@ class ItemViewSet(viewsets.ModelViewSet):
 
   #POST
   #define que se realizara al momento de utilizar metodo POST
+  #deberia agregar un item a la base de datos
   def create(self,request,*args,**kwargs):
     #recupera la api_key enviada por el usuario
     api_key = request.data.get('user_key',None)
@@ -29,6 +30,7 @@ class ItemViewSet(viewsets.ModelViewSet):
   
   #GET
   #define que se realizara al momento de utilizar metodo GET
+  #deberia retornar todos los items de cierto usuario
   def list(self, request, *args, **kwargs):
     #recupera la api key recepcionada comprobando de que exista
     api_key = request.GET.get('user_key',None)
@@ -42,6 +44,36 @@ class ItemViewSet(viewsets.ModelViewSet):
     queryset = self.queryset.filter(user_key=api_key)
     serializer = self.serializer_class(queryset,many=True)
     return Response(serializer.data)
+  
+  #PUT
+  #define que se realizara al momento de utilizar metodo PUT
+  #deberia actualizar un item solo si es del usuario
+  def update(self,request,*args,**kwargs):
+    item_id = kwargs.get('pk')
+    api_key = request.data.get('user_key',None)
+    if api_key is None:
+      return Response({'error': 'user_key is required'}, status=status.HTTP_400_BAD_REQUEST)
+    user = ExtendedUser.objects.filter(api_key=api_key).first()
+    if user is None:
+      return Response({'error': 'Invalid user_key'}, status=status.HTTP_403_FORBIDDEN)
+    item = self.queryset.get(pk=item_id)
+    if item.user_key!=api_key:
+      return Response({'error': 'Item doesnt belong to user'}, status=status.HTTP_403_FORBIDDEN)
+    return super().update(request, *args, **kwargs)
+  
+  def destroy(self,request,*args,**kwargs):
+    api_key = request.GET.get('user_key',None)
+    if api_key is None:
+      return Response({'error': 'user_key is required'}, status=status.HTTP_400_BAD_REQUEST)
+    user = ExtendedUser.objects.filter(api_key=api_key).first()
+    if user is None:
+      return Response({'error': 'Invalid user_key'}, status=status.HTTP_403_FORBIDDEN)
+    instance = self.get_object()
+    if instance.user_key != api_key:
+      return Response({'error': 'this item doenst belong to you'}, status=status.HTTP_403_FORBIDDEN)
+    self.perform_destroy(instance)
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 
